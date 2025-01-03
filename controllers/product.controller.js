9d6d8c7ec8aac0d7b802cbed973aca7b5f5185f9
Stanislas-Web <57170716+Stanislas-Web@users.noreply.github.com>
2025-01-03 22:05:01 +0100

    code update

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -45,7 +45,7 @@ exports.getProductsByCompanyId = async (req, res) => {
     if (!products || products.length === 0) {
       return res.status(404).json({ message: 'No products found for this company' });
     }
-    res.status(200).json({ products });
+    res.status(200).json( products );
   } catch (error) {
     res.status(500).json({ message: 'Error fetching products for company', error });
   }
